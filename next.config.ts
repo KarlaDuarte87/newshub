12f3b51f9817963c74947d5a87cbfe868162c223
Karla Duarte Ferreira <karladuarte87@hotmail.com>
2025-12-28 22:56:55 -0300

    Update configuration and dependencies for Next.js project; added autoprefixer, updated tailwindcss, and enabled dark mode in Tailwind config.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { resolve } from "path";
+
+// Obtém o diretório absoluto do projeto newshub
+const projectRoot = resolve(__dirname || process.cwd());
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    root: projectRoot,
+  },
 };
 
 export default nextConfig;
